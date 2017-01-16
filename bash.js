efe900198077fc286007086b664b5a8818de2c83
avarona <avarona10@gmail.com>
2017-01-16 13:46:28 -0500

    added request module

--- bash.js
+++ bash.js
@@ -1,22 +1,19 @@
-/* eslint-disable */
-
 let commands = require('./commands');
-
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', function(data) {
-  // let pwd = process.cwd();
-  // let date = new Date().toString().trim();
-  // if (cmd === 'pwd') process.stdout.write(pwd);
-  // if (cmd === 'date') process.stdout.write(date);
-
   let userCommand;
   let cmd = data.toString().trim();
   let cmdArray = cmd.split(' ');
 
   if (cmdArray[0] === 'echo') userCommand = 'echo';
-  else if (cmdArray[0] == 'cat') userCommand = 'cat';
+  else if (cmdArray[0] === 'cat') userCommand = 'cat';
+  else if (cmdArray[0] === 'head') userCommand = 'head';
+  else if (cmdArray[0] === 'tail') userCommand = 'tail';
+  else if (cmdArray[0] === 'curl') userCommand = 'curl';
   else userCommand = data.toString().trim();
 
   commands[userCommand](cmd);
+
+  // process.stdout.write('\nprompt > ');
 });
--- commands.js
+++ commands.js
@@ -1,5 +1,5 @@
-/* eslint-disable */
 let fs = require('fs');
+let request = require('./node_modules/request');
 
 let argsSplit = function(cmd) {
   let cmdArray = cmd.split(' ');
@@ -22,9 +22,9 @@ module.exports = {
     fs.readdir('.', function(err, files) {
       if (err) throw err;
       files.forEach(function(file) {
-        process.stdout.write(file.toString() + "\n");
+        process.stdout.write(file.toString() + '\n');
       });
-      process.stdout.write("prompt > ");
+      process.stdout.write('prompt > ');
     });
   },
   echo: function(cmd) {
@@ -37,19 +37,57 @@ module.exports = {
       });
     }
     process.stdout.write('\nprompt > ');
-    process.stdout.write("");
   },
   cat: function(cmd) {
     let args = argsSplit(cmd);
-    fs.readFile
-    // if (args[0] === '') {
-    //
-    // }
+    args.forEach(function(el) {
+      fs.readFile(el, function(err, contents) {
+        if (err) throw err;
+        process.stdout.write(contents);
+      });
+    })
+      process.stdout.write('\nprompt > ');
   },
-  head: function() {
-
+  head: function(cmd) {
+    let args = argsSplit(cmd);
+    fs.readFile(args[0], function(err, contents) {
+      if (err) throw err;
+      var arr = contents.toString().split('\n');
+      for (let i = 0; i < 5; i++) {
+        process.stdout.write(arr[i] + '\n');
+      }
+      process.stdout.write('prompt > ');
+    })
   },
-  tail: function() {
-
+  tail: function(cmd) {
+    let args = argsSplit(cmd);
+    fs.readFile(args[0], function(err, contents) {
+      if (err) throw err;
+      let arr = contents.toString().split('\n');
+      let length = arr.length;
+      for (let i = length - 5 ; i < length; i++) {
+        process.stdout.write(arr[i] + '\n');
+      }
+      process.stdout.write('prompt > ');
+    })
+  },
+  // TODO: Complete sort, wc & uniq functions
+//   sort: function(cmd) {
+//
+//   },
+//   wc: function(cmd) {
+//
+//   },
+//   uniq: function(cmd) {
+//
+//   }
+  curl: function(cmd) {
+    let args = argsSplit(cmd);
+    // need to handle for when http:// is not included
+    request(args[0], function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        console.log(body) // Show the HTML for the Google homepage.
+      }
+    })
   }
 }
